4c2184690a867191d3f7ea6efb3585dbf182f421
Jorge Teixeira <jorgeteixe11@gmail.com>
2024-04-18 01:00:16 +0300

    add necessary things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://pexegueiro.estudoslocais.org',
+  integrations: [tailwind(), mdx()]
+});
