9bf82a20b7d1e0ae0ce18066c15abc13b43f91d4
Jorge Teixeira <jorgeteixe11@gmail.com>
2024-07-24 22:16:18 +0200

    feat: many changes and first collections done

---
+++ src/content/config.ts
@@ -0,0 +1,22 @@
+import { defineCollection, z } from 'astro:content'
+
+const entradas = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    updatedDate: z.date(),
+  }),
+})
+
+const muinos = defineCollection({
+  type: 'content',
+  schema: z.object({
+    name: z.string(),
+    updatedDate: z.date(),
+  }),
+})
+
+export const collections = {
+  entradas: entradas,
+  muinos: muinos,
+}
